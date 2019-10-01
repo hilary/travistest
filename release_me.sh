b340f15afe439fbe1bb8687970f8381c789b7c0d
Hilary Holz <hholz@digitalocean.com>
2019-10-01 11:42:55 -0700

    testing github-changelog-generator

---
+++ release_me.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "generating changelog"
+tfile=$(mktemp /tmp/doctl-CHANGELOG-XXXXXX)
+github-changelog-generator -org digitalocean -repo doctl >"$tfile"
+
+cat "$tfile"
+
+goreleaser --rm-dist --release-notes="$tfile"
+
+echo "released!"
