3586bedcc8401849ba0ad7a3a48de0459a364e52
Hilary Holz <hholz@digitalocean.com>
2019-10-01 16:26:21 -0700

    exploring go module behavior under 1.13 on travis

--- release_me.sh
+++ release_me.sh
@@ -3,11 +3,20 @@
 set -euo pipefail
 
 echo "generating changelog"
-tfile=$(mktemp /tmp/doctl-CHANGELOG-XXXXXX)
+
+currentDir=$(pwd)
+
+# work in temp dir to prevent polluting our go.mod
+cd "$(mktemp -d)"
+
+go get -u github.com/digitalocean/github-changelog-generator
+
+tfile=$(mktemp)
 github-changelog-generator -org digitalocean -repo doctl >"$tfile"
 
 cat "$tfile"
 
+cd "$currentDir"
 goreleaser --rm-dist --release-notes="$tfile"
 
 echo "released!"
